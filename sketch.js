0f53062cffbd76a65fe2e50130ff8a9f2dfc87c2
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-11-22 17:18:46 -0800

    update

--- sketch.js
+++ sketch.js
@@ -5,22 +5,38 @@
 //game state
 //time score
 //buttons
+let gameState;
 let startButton;
 let toggleSound;
 let exitButton;
 let backgroundImage;
+let playerSprite, playerX = 100, playerY = 250, playerSpeed = 1;
+let peopleSprite, peopleX, peopleY, peopleSprites;
+let people = [];
+let hit = false;
 
 function preload() {
   backgroundImage = loadImage('images/background.png');
+  playerSprite = loadImage('images/playerSprite.png');
+  peopleSprite = loadImage('images/peopleSprite.png');
 }
 
 function setup() {
   createCanvas(1500, 500);
   background(50);
   titleScreen();
+  for (let i = 0; i < 400; i++) {
+    people.push(new peopleSpriteClass());
+  }
 }
 
 function draw() {
+  if (gameState === 1) {
+    gameScreen();
+  }
+
+
+
   //if game state is "titlescreen"
   //then execute the titlescreen function
 
@@ -41,6 +57,7 @@ function draw() {
 }
 
 function titleScreen() {
+  gameState = 0;
   image(backgroundImage, 0, 0);
   startButton = createButton('Play Game');
   startButton.size(200);
@@ -54,7 +71,6 @@ function titleScreen() {
   exitButton.size(200);
   exitButton.position(width/2-100, height*0.7);
   exitButton.mousePressed(exitGame);
-
   //display custom background
   //display game title
   //display button for "play game"
@@ -64,14 +80,58 @@ function titleScreen() {
 }
 
 function gameScreen() {
+  gameState = 1;
   background(200);
   text('gameScreen', width/2, height/3);
   startButton.hide();
   toggleSound.hide();
   exitButton.hide();
   //display image of player sprite
+  rect(playerX, playerY, 12, 20);
+  image(playerSprite, playerX, playerY);
+  if (keyIsDown(87)) {playerY -= playerSpeed;}//w
+  if (keyIsDown(65)) {playerX -= playerSpeed;}//a
+  if (keyIsDown(83)) {playerY += playerSpeed;}//s
+  if (keyIsDown(68)) {playerX += playerSpeed;}//d
   //display image of opposing people sprite
+  for (let i = 0; i < people.length; i++) {
+    people[i].move();
+    people[i].display();
+  }
 }
+
+class peopleSpriteClass {
+  constructor() {
+    this.x = 1500 + random(0, 1500);
+    this.y = random(0, height);
+    this.speed = 1;
+  }
+  move() {
+    this.x -= this.speed;
+    if (this.x <= -10) {
+      this.x = 1500;
+      this.y = random(0, height);
+    }
+  }
+  display() {
+    image(peopleSprite, this.x, this.y);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function pauseScreen() {
   text('Pause', width/2, height/2);
