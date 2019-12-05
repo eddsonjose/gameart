6f6d433d8332bae9e5220bdde721dd0b076017e2
eddsonjose <44173160+eddsonjose@users.noreply.github.com>
2019-12-04 22:21:03 -0800

    update

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ let description = [
   }
 ];
 let gameState, startButton, gameReset, moveControls;
-let backgroundImage, gameScreenBackground, clouds_overlay;
+let backgroundImage, gameScreenBackground, clouds_overlay, pause;
 let gameScreen_x = 0, gameScreen_x2, scrollSpeed = 0.5, clouds_overlay_x = -1500;
 let playerSprite, playerX = 100, playerY = 250, playerSpeed = 1;//0.5 original speed
 let peopleSprite, peopleSprite2, peopleSprite3;
@@ -21,7 +21,7 @@ let peopleSprite2Gif;
 let peopleX = 0, peopleY = 0, peopleXX = 0, peopleYY = 0, people = [], peopleTwo = [];
 let hit = false;
 let counter = 0;
-let score = 0;
+let score = 0, score2 = 0;
 let sky_0, sky_0_speed = 0.1, sky_0_x = 0, sky_0_x2;
 let sky_1, sky_1_speed = 0.2, sky_1_x = 0, sky_1_x2;
 let sky_2, sky_2_speed = 0.3, sky_2_x = 0, sky_2_x2;
@@ -35,6 +35,7 @@ function preload() {//createImg is for gif
   //background
   backgroundImage = loadImage('images/gameArt_background.png');
   gameScreenBackground = loadImage('images/gameScreen_background.png');
+  pause = loadImage('images/pause.png');
   clouds_overlay = loadImage('images/clouds_overlay.png');
   sky_0 = loadImage('images/sky_0.png');
   sky_1 = loadImage('images/sky_1.png');
@@ -52,10 +53,10 @@ function preload() {//createImg is for gif
 //-------------------------------------------------------------------------
 function setup() {
   createCanvas(1500, 500); background(50); titleScreen();
-  for (let i = 0; i < 80; i++) {//80 = hard
+  for (let i = 0; i < 60; i++) {//80 = hard
     people.push(new peopleSpriteClass());
   }
-  for (let i = 0; i < 200; i++) {//200 = hard
+  for (let i = 0; i < 150; i++) {//200 = hard
     peopleTwo.push(new peopleSpriteClassTwo());
   }
   playerSprite.hide();
@@ -83,12 +84,12 @@ function titleScreen() {
   startButton = createButton('Press "Enter" to Play');
   startButton.size(200);
   startButton.style('width:800px');
-  startButton.position(width/4, height/2.2);
+  startButton.position(width/4, height*0.56);
   startButton.mousePressed(gameScreen);
-  gameReset = createButton('Press "F5" for Main Menu');
-  gameReset.size(200);
-  gameReset.style('width:800px');
-  gameReset.position(width/4, height*0.55);
+  // gameReset = createButton('Press "F5" for Main Menu');
+  // gameReset.size(200);
+  // gameReset.style('width:800px');
+  // gameReset.position(width/4, height*0.55);
   // moveControls = createButton('"WASD" to Move');
   // moveControls.size(200);
   // moveControls.position(width/2-80, height*0.99);
@@ -98,7 +99,8 @@ function titleScreen() {
 function gameScreen() {
   gameState = 1;
   //hide buttons
-  startButton.hide(); gameReset.hide();
+  startButton.hide();
+  // gameReset.hide();
   // moveControls.hide();
   //display image of player sprite
   fill(0, 0);
@@ -113,7 +115,7 @@ function gameScreen() {
   if (keyIsDown(80)) {//p, pauseScreen
     gameState = 2;
     startButton.show();
-    gameReset.show();
+    // gameReset.show();
   }
   //display image of people sprite
   for (let i = 0; i < people.length; i++) {
@@ -127,8 +129,9 @@ function gameScreen() {
   //score/nervousness
   fill(255); noStroke(); textSize(24); textAlign(RIGHT);
   text(score + playerX - 100 + ' nervousness', 1480, 25);
+  score2 = score + playerX - 100
   counter++;
-  if (counter === 60) {score += 7; counter = 0;}
+  if (counter === 60) {score += 10; counter = 0;}
   //collisions
   if(playerX <= 1) {playerX = 2;}
   else if (playerX >= 1490) {playerX--; end();}
@@ -148,6 +151,15 @@ function gameScreen() {
   text('A person with social anxiety may practice engaging in social interactions to decrease their feelings of nervousness. Avoiding social interactions increases nervousness. The current is endless. Define your own goal.', description_x, 90);
   description_x -= 0.5;
   if (description_x < -8000) {description_x = 3000;}
+  //player's speed doubles if nervousness is below -100
+  textSize(24);
+  if (score2 < -100) {
+    text('Social interactions are now easier', 15, 25);
+    playerSpeed = 2;
+  } else {
+    playerSpeed = 1;
+  }
+  //endgame
 }
 //-------------------------------------------------------------------------
 //-------------------------------------------------------------------------
@@ -203,10 +215,12 @@ function end() {
 //-------------------------------------------------------------------------
 //-------------------------------------------------------------------------
 function pauseScreen() {
+  image(pause, 0, 0);
   gameState = 2;
-  fill('#33302a'); textSize(42);
-  text('P A U S E', width/2 - 100, height/2 - 75);
-  startButton.show(); gameReset.show();
+  // fill('#33302a'); textSize(42);
+  // text('P A U S E', width/2 - 100, height/2 - 75);
+  startButton.show();
+  // gameReset.show();
   playerSprite.hide();
   // moveControls.show();
   //pause gameScreen
